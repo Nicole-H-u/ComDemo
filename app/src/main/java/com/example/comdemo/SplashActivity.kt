8c5d1b8652c9516b90ace867a76b6add1b091f1c
Nicole-H-u <1732781573@qq.com>
2022-05-27 19:40:46 +0800

    add

--- app/src/main/java/com/example/comdemo/SplashActivity.kt
+++ app/src/main/java/com/example/comdemo/SplashActivity.kt
@@ -2,15 +2,18 @@ package com.example.comdemo
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import com.example.comdemo.databinding.ActivitySplashBinding
 import com.example.common.base.BaseActivity
+import com.example.login.view.LoginActivity
 
 class SplashActivity : BaseActivity<ActivitySplashBinding>() {
 
     override fun initData(savedInstanceState: Bundle?) {
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-        finish()
+        Handler().postDelayed(Runnable {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()}, 3000)
     }
 
     override fun getLayoutId(): Int = R.layout.activity_splash
